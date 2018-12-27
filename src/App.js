d0adc9057e1bfdacb8d73994ad01ba4db05fe2a2
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2018-12-28 00:58:16 +0530

    SearchBox working

--- src/App.js
+++ src/App.js
@@ -19,6 +19,17 @@ const particleOptions={
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input:''
+    }
+  }
+
+  onInputChange=(event)=>{
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
@@ -29,7 +40,7 @@ class App extends Component {
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm onInputChange={this.onInputChange}/>
         {/* 
         <FaceRecognition/>} */}
       </div>
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -9,7 +9,7 @@ class ImageLinkForm extends Component{
           </p>
           <div className='center'>
             <div className='form center pa4 br3 shadow-5'>
-              <input className='f4 pa2 w-70 center' type="tex"></input>
+              <input className='f4 pa2 w-70 center' type="tex" onChange={this.props.onInputChange}></input>
               <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
             </div>
           </div>
