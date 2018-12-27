0d507a1e96b75b92770b6d249fbf82a434472bd9
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2018-12-28 00:43:01 +0530

    Added components

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+class Rank extends Component{
+  render(){
+    return(
+      <div>
+        <div className='white f2'>
+          {'Andrei, your current rank is...'}
+        </div>
+        <div className='white f1'>
+          {'#5'}
+        </div>
+      </div>
+    )
+  }
+}
+export default Rank;
